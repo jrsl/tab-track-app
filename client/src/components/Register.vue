375a622da4718c47836cc6b7859b5dd201ed4853
Juan Lopes <juan@bolden.nl>
2018-01-18 15:26:22 +0100

    Implementing authentication

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -5,6 +5,7 @@
     <br>
     <input type="password" name="password" placeholder="password" v-model="password">
     <br>
+    <div class="error" v-html="error"></div>
     <button @click="register">Register</button>
   </div>
 </template>
@@ -16,19 +17,27 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+.error {
+  color: red;
+}
 </style>
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,9 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
 module.exports = (app) => {
-  app.post('/register', (req, res) => {
-    res.send({
-      message: `Hello ${req.body.email}! Your user was registered! have fun!`
-    })
-  })
+  app.post('/register',
+    AuthenticationControllerPolicy.register,
+    AuthenticationController.register
+  )
 }
